a92b4a3a0e71607275284f8a9fd4d736585bb090
Ravikant Saini <raviblog.tech@gmail.com>
2024-02-14 21:46:03 +0530

    Update .gitignore and settings.py***

--- IgCMS/settings.py
+++ IgCMS/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'Home/static'
 ]
 
-MEDIA_ROOT = BASE_DIR / 'media' #TODO: Uncomment this line, and the next one, to enable media files
+MEDIA_ROOT = BASE_DIR / 'media' 
 MEDIA_URL = 'media/'
 
 # Default primary key field type
